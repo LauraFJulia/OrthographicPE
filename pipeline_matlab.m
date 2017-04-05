375d93272fc4dcd83f87ea1f8179e83e2497a7a7
LauraFJulia <fernandl@imagine.enpc.fr>
2017-04-05 15:31:29 +0200

    cleaned script

--- pipeline_matlab.m
+++ pipeline_matlab.m
@@ -1,12 +1,6 @@
 
 clear;
 
-%% Only if using Octave 
-% load packages
-pkg load statistics;
-pkg load optim;
-
-
 %% Dataset info %%%
 im_path='data/';
 images_names={'_MG_0441.JPG','_MG_0445.JPG','_MG_0447.JPG'};
@@ -39,10 +33,12 @@ corresp13=reshape(corresp13,4,[]);
 
 %% Compute tracks %%%
 Corresp=matches2triplets(corresp12,corresp23,corresp13);
+fprintf('%d tracks between the three images.\n',size(Corresp,2));
 
 
 %% A C RANSAC with Orthographic model %%%
 [inliers,Sol,ransac_th]=AC_RANSAC_Orthographic(Corresp,CalM,imsize);
+fprintf('%d inliers were found by AC-RANSAC.\n',length(inliers));
 
 %% Orthographic model with all inliers %%%
 [Sol1,Sol2]=OrthographicPoseEstimation(Corresp(:,inliers),CalM);
@@ -50,15 +46,21 @@ Corresp=matches2triplets(corresp12,corresp23,corresp13);
 %% B A for both possible solutions %%%
 R_t_0=[Sol1{1},Sol1{2}];
 [R_t_1,Reconst1,iter1,repr_err1]=BundleAdjustment(CalM,R_t_0,Corresp(:,inliers));
+fprintf('Minimum reached for first solution with %d iterations. ',iter1);
+fprintf('Final reprojection error is %f.\n',repr_err1);
 
 R_t_0=[Sol2{1},Sol2{2}];
 [R_t_2,Reconst2,iter2,repr_err2]=BundleAdjustment(CalM,R_t_0,Corresp(:,inliers));
+fprintf('Minimum reached for second solution with %d iterations. ',iter2);
+fprintf('Final reprojection error is %f.\n',repr_err2);
 
 %% Choose solution with less repr. err. %%%
 if repr_err1<repr_err2
     Solution=R_t_1;
+    Reconst=Reconst1;
 else
     Solution=R_t_2;
+    Reconst=Reconst2;
 end
 
 %% Orientations 
