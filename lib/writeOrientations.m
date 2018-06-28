6127a1cd12bcc4d8833ad766e31fc2eb314f2c44
Pascal Monasse <monasse@imagine.enpc.fr>
2018-06-28 14:32:22 +0200

    Clearer layout for Rt matrices

--- lib/writeOrientations.m
+++ lib/writeOrientations.m
@@ -25,10 +25,14 @@ function writeOrientations(file_name,R_t)
 
 M=size(R_t,1)/3;
 file = fopen(file_name, 'wt');
-fprintf(file,'Orientation file. Total of %d cameras.\n',M);
-fprintf(file,'Format: 3 lines per camera, 3 first columns for rotation, last column for translation.\n\n');
-for i=1:size(R_t,1)
-    fprintf(file,'%f %f %f %f\n', R_t(i,1),R_t(i,2),R_t(i,3),R_t(i,4));
+fprintf(file,'%% [R t] matrices of %d cameras.\n',M);
+for m=1:M
+    fprintf(file, 'P%d=[\n', m-1);
+    for j=1:3
+        i=3*(m-1)+j;
+        fprintf(file,'     %f %f %f %f\n', R_t(i,1),R_t(i,2),R_t(i,3),R_t(i,4));
+    end
+    fprintf(file, ']\n');
 end
 fclose(file);
 
